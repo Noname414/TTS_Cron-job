b6f73a56f133b4e70381da53eaede4a6d5210ede
Noname414 <e6902015@gmail.com>
2025-07-10 12:17:09 +0800

    新增專案初始檔案，包括 .gitignore、Python 版本設定、播客腳本、主要程式碼、依賴管理檔案及 GitHub 工作流程設定，實現每日自動生成播客功能。

---
+++ main.py
@@ -0,0 +1,58 @@
+from google import genai
+from google.genai import types
+import wave
+
+# Set up the wave file to save the output:
+def wave_file(filename, pcm, channels=1, rate=24000, sample_width=2):
+   with wave.open(filename, "wb") as wf:
+      wf.setnchannels(channels)
+      wf.setsampwidth(sample_width)
+      wf.setframerate(rate)
+      wf.writeframes(pcm)
+      print(f"Audio saved to {filename}")
+
+def generate_audio(script):
+    client = genai.Client()
+    prompt = "以中文Podcast方式朗誦以下對話，並以主持人林冠傑和林欣潔的口吻："+script
+
+    response = client.models.generate_content(
+    model="gemini-2.5-flash-pro-tts",
+    contents=prompt,
+    config=types.GenerateContentConfig(
+        response_modalities=["AUDIO"],
+        speech_config=types.SpeechConfig(
+            multi_speaker_voice_config=types.MultiSpeakerVoiceConfig(
+                speaker_voice_configs=[
+                types.SpeakerVoiceConfig(
+                    speaker='林冠傑',
+                    voice_config=types.VoiceConfig(
+                        prebuilt_voice_config=types.PrebuiltVoiceConfig(
+                            voice_name='Charon',
+                        )
+                    )
+                ),
+                types.SpeakerVoiceConfig(
+                    speaker='林欣潔',
+                    voice_config=types.VoiceConfig(
+                        prebuilt_voice_config=types.PrebuiltVoiceConfig(
+                            voice_name='Zephyr',
+                        )
+                    )
+                ),
+                ]
+            )
+        )
+    )
+    )
+
+    data = response.candidates[0].content.parts[0].inline_data.data
+
+    file_name='out.wav'
+    wave_file(file_name, data) # Saves the file to current directory
+
+if __name__ == "__main__":
+    print("Generating audio...")
+    with open('2507.05257v1_script.txt', 'r', encoding='utf-8') as file:
+        script = file.read()
+    generate_audio(script)
+    print("Audio generated successfully!")
